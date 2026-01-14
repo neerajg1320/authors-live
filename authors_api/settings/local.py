ecf1ed4cbf3494a64c3227f45f5c1b28c5dc33f3
Neeraj Gupta <neerajg1320@gmail.com>
2026-01-14 11:31:41 +0530

    First commit

---
+++ authors_api/settings/local.py
@@ -0,0 +1,21 @@
+from .base import *  # noqa
+from .base import env
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# python -c "import secrets; print(secrets.token_urlsafe(38))"
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="Dhe--ysJrR8yLE43EIpefuxXCNBonnIWk3qo83rmsTBsUz8GVmg",
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
+# EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+# EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+# EMAIL_PORT = env("EMAIL_PORT")
+# DEFAULT_FROM_EMAIL = "support@apiimperfect.site"
+# DOMAIN = env("DOMAIN")
+# SITE_NAME = "Authors Haven"
